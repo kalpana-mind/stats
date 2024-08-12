424ecd6a67db31aa31c55c90dddd7712307f2999
kalpana-mind <152434265+kalpana-mind@users.noreply.github.com>
2024-08-11 20:42:01 -0400

    'Final commit'

--- build/index.js
+++ build/index.js
@@ -1,16 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const MatchResult_1 = require("./MatchResult");
-const reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
-let manUnitedWins = 0;
-for (let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(`Man United won ${manUnitedWins} games`);
+const Summary_1 = require("./Summary");
+const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
+const summary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
+matchReader.load();
+summary.buildAndPrintReport(matchReader.matches);
